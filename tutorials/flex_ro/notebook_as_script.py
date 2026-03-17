73ed74fdea532c014ad1f6a96d8553be1246dba1
kurbansitterley <kurban.sitterley@nrel.gov>
2026-03-17 09:44:02 -0600

    MIPGap

--- tutorials/flex_ro/notebook_as_script.py
+++ tutorials/flex_ro/notebook_as_script.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     mip_gap = 0.03
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     # solver = pyo.SolverFactory("gurobi_persistent")
-    # solver.options["MIPGap"] = mip_gap
+    solver.options["MIPGap"] = mip_gap
     # solver.set_instance(m)
     results = solver.solve(m, tee=True)
 

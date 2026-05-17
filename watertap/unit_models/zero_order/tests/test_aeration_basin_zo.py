e175980495455087b082801210c51f4cdb23307f
kurbansitterley <kurban.sitterley@nrel.gov>
2026-05-17 13:15:35 -0600

    update costing year, add reference, add costing test

--- watertap/unit_models/zero_order/tests/test_aeration_basin_zo.py
+++ watertap/unit_models/zero_order/tests/test_aeration_basin_zo.py
@@ -22,6 +22,7 @@ from pyomo.environ import (
     value,
     Var,
     assert_optimal_termination,
+    units as pyunits,
 )
 from pyomo.util.check_units import assert_units_consistent
 
@@ -330,6 +331,7 @@ def test_costing():
     m.fs.params = WaterParameterBlock(solute_list=["sulfur", "toc", "tss"])
 
     m.fs.costing = ZeroOrderCosting()
+    m.fs.costing.base_currency = pyunits.USD_2023
 
     m.fs.unit1 = AerationBasinZO(property_package=m.fs.params, database=m.db)
 
@@ -341,7 +343,13 @@ def test_costing():
     assert degrees_of_freedom(m.fs.unit1) == 0
 
     m.fs.unit1.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+    m.fs.costing.cost_process()
+    m.fs.costing.add_LCOW(m.fs.unit1.properties_in[0].flow_vol)
+    assert_units_consistent(m.fs)
+    assert degrees_of_freedom(m.fs.unit1) == 0
 
+    results = solver.solve(m)
+    assert_optimal_termination(results)
     assert isinstance(m.fs.costing.aeration_basin, Block)
     assert isinstance(m.fs.costing.aeration_basin.capital_a_parameter, Var)
     assert isinstance(m.fs.costing.aeration_basin.capital_b_parameter, Var)
@@ -349,8 +357,8 @@ def test_costing():
 
     assert isinstance(m.fs.unit1.costing.capital_cost, Var)
     assert isinstance(m.fs.unit1.costing.capital_cost_constraint, Constraint)
-
-    assert_units_consistent(m.fs)
-    assert degrees_of_freedom(m.fs.unit1) == 0
-
+    assert pytest.approx(value(m.fs.costing.LCOW), rel=1e-3) == 0.03945
+    assert (
+        pytest.approx(value(m.fs.costing.total_capital_cost), rel=1e-3) == 28854446.35
+    )
     assert m.fs.unit1.electricity[0] in m.fs.costing._registered_flows["electricity"]

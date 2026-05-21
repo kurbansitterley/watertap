7cd2f0ed923316e23a99598f3a107b8ea9581e84
kurbansitterley <kurban.sitterley@nrel.gov>
2026-05-21 06:45:17 -0600

    update costing test

--- watertap/unit_models/zero_order/tests/test_air_flotation_zo.py
+++ watertap/unit_models/zero_order/tests/test_air_flotation_zo.py
@@ -278,26 +278,25 @@ def test_costing():
 
     m.fs = FlowsheetBlock(dynamic=False)
 
-    m.fs.params = WaterParameterBlock(solute_list=["sulfur", "toc", "tss"])
+    m.fs.params = WaterParameterBlock(solute_list=["tss"])
 
     m.fs.costing = ZeroOrderCosting()
-    m.fs.costing.base_currency = pyunits.USD_2023
+    m.fs.costing.base_currency = pyunits.USD_2007
 
-    m.fs.unit1 = AirFlotationZO(property_package=m.fs.params, database=m.db)
+    m.fs.unit = AirFlotationZO(property_package=m.fs.params, database=m.db)
+    rho = 1000 * pyunits.kg / pyunits.m**3
+    flow_vol = 185 * pyunits.Mgallons / pyunits.day
+    flow_mass = rho * flow_vol
 
-    m.fs.unit1.inlet.flow_mass_comp[0, "H2O"].fix(10000)
-    m.fs.unit1.inlet.flow_mass_comp[0, "sulfur"].fix(1)
-    m.fs.unit1.inlet.flow_mass_comp[0, "toc"].fix(2)
-    m.fs.unit1.inlet.flow_mass_comp[0, "tss"].fix(3)
-    m.fs.unit1.load_parameters_from_database(use_default_removal=True)
-    assert degrees_of_freedom(m.fs.unit1) == 0
-
-    m.fs.unit1.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+    m.fs.unit.inlet.flow_mass_comp[0, "H2O"].fix(flow_mass)
+    m.fs.unit.inlet.flow_mass_comp[0, "tss"].fix(0.01)
+    m.fs.unit.load_parameters_from_database(use_default_removal=True)
+    m.fs.unit.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
 
     m.fs.costing.cost_process()
-    m.fs.costing.add_LCOW(m.fs.unit1.properties_in[0].flow_vol)
+    m.fs.costing.add_LCOW(m.fs.unit.properties_in[0].flow_vol)
     assert_units_consistent(m.fs)
-    assert degrees_of_freedom(m.fs.unit1) == 0
+    assert degrees_of_freedom(m.fs.unit) == 0
 
     results = solver.solve(m)
     assert_optimal_termination(results)
@@ -307,12 +306,13 @@ def test_costing():
     assert isinstance(m.fs.costing.air_flotation.capital_b_parameter, Var)
     assert isinstance(m.fs.costing.air_flotation.reference_state, Var)
 
-    assert isinstance(m.fs.unit1.costing.capital_cost, Var)
-    assert isinstance(m.fs.unit1.costing.capital_cost_constraint, Constraint)
+    assert isinstance(m.fs.unit.costing.capital_cost, Var)
+    assert isinstance(m.fs.unit.costing.capital_cost_constraint, Constraint)
 
-    assert pytest.approx(value(m.fs.costing.LCOW), rel=1e-3) == 0.027124
+    assert pytest.approx(value(m.fs.costing.LCOW), rel=1e-3) == 0.0179298
     assert (
-        pytest.approx(value(m.fs.costing.total_capital_cost), rel=1e-3) == 14669149.45
-    )
+        pytest.approx(value(m.fs.unit.costing.direct_capital_cost), rel=1e-3)
+        == 2250994.09
+    )  # ~$2.299M from reference
 
-    assert m.fs.unit1.electricity[0] in m.fs.costing._registered_flows["electricity"]
+    assert m.fs.unit.electricity[0] in m.fs.costing._registered_flows["electricity"]

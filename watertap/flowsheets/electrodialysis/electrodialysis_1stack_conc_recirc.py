85eecbdf66d008d320763c59d96a872531ddd5b0
kurbansitterley <kurban.sitterley@nrel.gov>
2026-06-13 13:07:50 -0600

    scale conc_mol_phase

--- watertap/flowsheets/electrodialysis/electrodialysis_1stack_conc_recirc.py
+++ watertap/flowsheets/electrodialysis/electrodialysis_1stack_conc_recirc.py
@@ -126,6 +126,8 @@ def main(ED_1D=True):
     m.fs.prod.properties[0].conc_mol_phase_comp["Liq", "Na_+"].fix(
         1.7094
     )  # Corresponding to C_product = 100 ppm
+    m.fs.prod.properties[0].conc_mol_phase_comp
+    iscale.calculate_scaling_factors(m)
     m.fs.objective = Objective(expr=m.fs.costing.LCOW)
     solve(m, solver=solver, tee=True)
     m.fs.EDstack.cell_pair_num.fix(round(m.fs.EDstack.cell_pair_num.value))
@@ -402,8 +404,8 @@ def initialize_system(m, solver=None, ED_1D=True):
     iscale.set_scaling_factor(m.fs.EDstack.cell_width, 5)
     iscale.set_scaling_factor(m.fs.EDstack.cell_length, 1)
     iscale.set_scaling_factor(m.fs.EDstack.cell_pair_num, 0.1)
-    iscale.set_scaling_factor(m.fs.pump0.control_volume.work, 1e1)
-    iscale.set_scaling_factor(m.fs.pump1.control_volume.work, 1e1)
+    iscale.set_scaling_factor(m.fs.pump0.control_volume.work, 1)
+    iscale.set_scaling_factor(m.fs.pump1.control_volume.work, 1)
     iscale.calculate_scaling_factors(m)
     iscale.constraint_scaling_transform(
         m.fs.eq_recovery_vol_H2O,
@@ -576,3 +578,6 @@ def display_model_metrics(m, ED_1D=True):
 
 if __name__ == "__main__":
     m = main(ED_1D=True)
+    m = main(ED_1D=False)
+    from watertap.kurby import *
+    check_scaling(m)

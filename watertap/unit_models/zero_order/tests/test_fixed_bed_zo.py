7280dfbe6872280afc9b1fda44c4b452f5f924cf
kurbansitterley <kurban.sitterley@nrel.gov>
2026-06-16 22:30:12 -0600

    classmethod

--- watertap/unit_models/zero_order/tests/test_fixed_bed_zo.py
+++ watertap/unit_models/zero_order/tests/test_fixed_bed_zo.py
@@ -42,7 +42,8 @@ solver = get_solver()
 
 class TestFixedBedZO_w_o_default_removal:
     @pytest.fixture(scope="class")
-    def model(self):
+    @classmethod
+    def model(cls):
         m = ConcreteModel()
         m.db = Database()
 
@@ -215,7 +216,8 @@ class TestFixedBedZO_w_o_default_removal:
 
 class TestFixedBedZO_w_default_removal:
     @pytest.fixture(scope="class")
-    def model(self):
+    @classmethod
+    def model(cls):
         m = ConcreteModel()
         m.db = Database()
 
@@ -366,7 +368,8 @@ params = db._get_technology("fixed_bed")
 @pytest.mark.component
 class TestFixedBedZOsubtype:
     @pytest.fixture(scope="class")
-    def model(self):
+    @classmethod
+    def model(cls):
         m = ConcreteModel()
 
         m.fs = FlowsheetBlock(dynamic=False)

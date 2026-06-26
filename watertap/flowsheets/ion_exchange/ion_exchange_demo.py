ffe24b494a5408274d86a768b3dabe985f1961a2
kurbansitterley <kurban.sitterley@nrel.gov>
2026-06-26 14:37:36 -0600

    revert IX demo

--- watertap/flowsheets/ion_exchange/ion_exchange_demo.py
+++ watertap/flowsheets/ion_exchange/ion_exchange_demo.py
@@ -9,60 +9,169 @@
 # information, respectively. These files are also available online at the URL
 # "https://github.com/watertap-org/watertap/"
 #################################################################################
+from pyomo.environ import (
+    ConcreteModel,
+    Objective,
+    assert_optimal_termination,
+    TransformationFactory,
+    units as pyunits,
+)
+from pyomo.network import Arc
 
-import pytest
+from idaes.core import FlowsheetBlock, UnitModelCostingBlock
+from idaes.core.util.model_statistics import degrees_of_freedom
+from idaes.core.util.scaling import calculate_scaling_factors
+from idaes.core.util.initialization import propagate_state
+from idaes.models.unit_models import Product, Feed
 
-from pyomo.util.check_units import assert_units_consistent
-import pyomo.environ as pyo
-import idaes.core as idc
+from watertap.core.util.initialization import check_dof
+from watertap.property_models.multicomp_aq_sol_prop_pack import MCASParameterBlock
+from watertap.unit_models.ion_exchange_0D import IonExchange0D
+from watertap.costing import WaterTAPCosting
+from watertap.core.solvers import get_solver
 
-from watertap.costing.watertap_costing_package import WaterTAPCosting
-import watertap.flowsheets.lsrro.lsrro as lsrro
+import math
+
+solver = get_solver()
 
 
-@pytest.mark.component
-def test_watertap_costing_package():
-    m = pyo.ConcreteModel()
-    m.fs = idc.FlowsheetBlock(dynamic=False)
+def main():
+    # The IX model currently only supports one "target" ion (i.e., the component in the water source that can be removed by IX)
+    # All other ions are inert. This demo does not contain inert ions, but an example can be found in the IX test file:
+    # watertap/watertap/unit_models/tests/test_ion_exchange_0D.py
+    target_ion = "Ca_2+"
+    ions = [target_ion]
 
+    # See ix_build for details on building the model for this demo.
+    m = ix_build(ions)
+    # See set_operating_conditions for details on operating conditions for this demo.
+    set_operating_conditions(m)
+    # See initialize_system for details on initializing the models for this demo.
+    initialize_system(m)
+    # Check the degrees of freedom of the model to ensure it is zero.
+    check_dof(m)
+    # Solve the model. Store the results in a local variable.
+    results = solver.solve(m)
+    # Ensure the solve resulted in an optimal termination status.
+    assert_optimal_termination(results)
+    # Display the degrees of freedom, termination status, and performance metrics of the model.
+    print(f"\nDOF = {degrees_of_freedom(m)}")
+    print(f"Model solve {results.solver.termination_condition.swapcase()}")
+    display_results(m)
+
+    # See optimize_system for details on optimizing this model for a specific condition.
+    optimize_system(m)
+    ix = m.fs.ion_exchange
+
+    # With our model optimized to new conditions in optimize_system,
+    # we can get the new number_columns and bed_depth and fix them in our model.
+    num_col = math.ceil(
+        ix.number_columns()
+    )  # To eliminate fractional number of columns
+    bed_depth = ix.bed_depth()
+    ix.bed_depth.fix(bed_depth)
+    ix.number_columns.fix(num_col)
+    check_dof(m)
+    results = solver.solve(m)
+    assert_optimal_termination(results)
+    print(f"\nDOF = {degrees_of_freedom(m)}")
+    print(f"Model solve {results.solver.termination_condition.swapcase()}")
+    display_results(m)
+
+    return m
+
+
+def ix_build(ions, target_ion=None, hazardous_waste=False, regenerant="NaCl"):
+
+    if not target_ion:
+        target_ion = ions[0]
+
+    # Create the model and flowsheet
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(dynamic=False)
+
+    # ion_config is the dictionary needed to configure the property package.
+    # For this demo, we only have properties related to the target ion (Ca_2+) and water (H2O)
+    ion_props = get_ion_config(ions)
+
+    # The water property package used for the ion exchange model is the multi-component aqueous solution (MCAS) property package
+    m.fs.properties = MCASParameterBlock(**ion_props)
+
+    # Add feed and product blocks to the flowsheet
+    # These are the unit models on the flowsheet that the source water "flows" from/to
+    # The must use the same property package as the ion exchange model
+    m.fs.feed = Feed(property_package=m.fs.properties)
+    m.fs.product = Product(property_package=m.fs.properties)
+    m.fs.regen = Product(property_package=m.fs.properties)
+
+    # Configuration dictionary used to instantiate the ion exchange model:
+    #   "property_package" indicates which property package to use for the ion exchange model
+    #   "target_ion" indicates which ion in the property package will be the reactive ion for the ion exchange model
+    #   "hazardous_waste" indicates if the regeneration and spent resin is considered hazardous. If so, it adds costs
+    #   "regenerant" indicates the chemical used to regenerate the ion exchange process
+    ix_config = {
+        "property_package": m.fs.properties,
+        "target_ion": target_ion,
+        "hazardous_waste": hazardous_waste,
+        "regenerant": regenerant,
+    }
+
+    # Add the ion exchange model to the flowsheet
+    m.fs.ion_exchange = ix = IonExchange0D(**ix_config)
+
+    add_costing(m)
+
+    # Touch properties so they are available for scaling, initialization, and reporting.
+    ix.process_flow.properties_in[0].conc_mass_phase_comp[...]
+    ix.process_flow.properties_out[0].conc_mass_phase_comp[...]
+    ix.regeneration_stream[0].conc_mass_phase_comp[...]
+    m.fs.feed.properties[0].flow_vol_phase[...]
+    m.fs.feed.properties[0].conc_mass_phase_comp[...]
+    m.fs.product.properties[0].conc_mass_phase_comp[...]
+
+    # Arcs are used to "connect" Ports on unit process models to Ports on other unit process models
+    # For example, in this next line the outlet Port on the Feed model is connected to the inlet Port on the ion exchange model
+    m.fs.feed_to_ix = Arc(source=m.fs.feed.outlet, destination=ix.inlet)
+    m.fs.ix_to_product = Arc(source=ix.outlet, destination=m.fs.product.inlet)
+    m.fs.ix_to_regen = Arc(source=ix.regen, destination=m.fs.regen.inlet)
+
+    TransformationFactory("network.expand_arcs").apply_to(m)
+
+    # Scaling variables in the model
+    # Here, the molar flow for water ("flow_mol_phase_comp[Liq, H2O]") on the Feed block is scaled by 1e-4.
+    # This is because the molar flow rate of water in this demo is ~2777 mol/s
+    # and scaling factors are chosen such that the value of the variable multiplied by the scaling factor is ~1
+    m.fs.properties.set_default_scaling(
+        "flow_mol_phase_comp", 1e-4, index=("Liq", "H2O")
+    )
+    m.fs.properties.set_default_scaling(
+        "flow_mol_phase_comp", 10, index=("Liq", target_ion)
+    )
+    # Call calculate_scaling_factors to apply scaling factors for each variable that we haven't set scaling factors for above.
+    calculate_scaling_factors(m)
+
+    return m
+
+
+def add_costing(m):
+    # Add the flowsheet level costing package
     m.fs.costing = WaterTAPCosting()
-
-    m.fs.electricity = pyo.Var(units=pyo.units.kW, initialize=1)
-
-    m.fs.costing.cost_flow(m.fs.electricity, "electricity")
-
-    assert "foo" not in m.fs.costing.flow_types
-    with pytest.raises(
-        ValueError,
-        match="foo is not a recognized flow type. Please check "
-        "your spelling and that the flow type has been registered with"
-        " the FlowsheetCostingBlock.",
-    ):
-        m.fs.costing.cost_flow(m.fs.electricity, "foo")
-
-    m.fs.costing.foo_cost = foo_cost = pyo.Var(
-        initialize=42,
-        doc="foo",
-        units=pyo.units.USD_2020 / pyo.units.m / pyo.units.second,
+    # Add costing blocks to the flowsheet
+    # Here, the ion exchange model has its own unit-level costing Block
+    m.fs.ion_exchange.costing = UnitModelCostingBlock(
+        flowsheet_costing_block=m.fs.costing  # Indicating which flowsheet costing block to use to aggregate unit-level costs to the system-level costs
     )
-
-    m.fs.costing.register_flow_type("foo", m.fs.costing.foo_cost)
-
-    # make sure the component was not replaced
-    # by register_defined_flow
-    assert foo_cost is m.fs.costing.foo_cost
-
-    m.fs.foo = pyo.Var(units=pyo.units.m, initialize=10)
-
-    m.fs.costing.cost_flow(m.fs.foo, "foo")
-
-    m.fs.costing.bar_base_cost = pyo.Var(
-        initialize=0.42,
-        doc="bar",
-        units=pyo.units.USD_2020 / pyo.units.g / pyo.units.hour,
+    # Call cost_process() method to create system-wide global parameters and add aggregating constraints to costing model
+    m.fs.costing.cost_process()
+    # Designate the volumetric flow on the Product block to be the stream used as the annual water production
+    m.fs.costing.add_annual_water_production(
+        m.fs.product.properties[0].flow_vol_phase["Liq"]
     )
-    m.fs.costing.bar_purity = pyo.Param(
-        initialize=0.50, doc="bar purity", units=pyo.units.dimensionless
+    # Add LCOW variable to costing block
+    m.fs.costing.add_LCOW(m.fs.product.properties[0].flow_vol_phase["Liq"])
+    # Add specific energy consumption variable to costing block
+    m.fs.costing.add_specific_energy_consumption(
+        m.fs.product.properties[0].flow_vol_phase["Liq"]
     )
     # Add breakdown of NaCl usage per unit product flow to costing package with name "regenerant_usage"
     m.fs.costing.add_flow_component_breakdown(
@@ -71,52 +180,56 @@ def test_watertap_costing_package():
         name="regenerant_usage",
     )
 
-    m.fs.costing.register_flow_type(
-        "bar", m.fs.costing.bar_base_cost * m.fs.costing.bar_purity
+
+def set_operating_conditions(m, flow_in=0.05, conc_mass_in=0.1, solver=None):
+    if solver is None:
+        solver = get_solver()
+    ix = m.fs.ion_exchange
+    target_ion = ix.config.target_ion
+
+    m.fs.feed.properties.calculate_state(
+        var_args={
+            ("flow_vol_phase", "Liq"): flow_in,  # m3/s
+            ("conc_mass_phase_comp", ("Liq", target_ion)): conc_mass_in,  # kg/m3
+            ("pressure", None): 101325,
+            ("temperature", None): 298,
+        },
+        hold_state=True,
     )
+    # Fix key decision variables in ion exchange model.
+    ix.langmuir[target_ion].fix(0.7)
+    ix.resin_max_capacity.fix(3)
+    ix.service_flow_rate.fix(15)
+    # Note: number_columns and bed_depth are typically not known a priori for this model.
+    # They can be determined by first running the model without these variables fixed.
+    ix.number_columns.fix(4)
+    ix.bed_depth.fix(1.7)
 
-    bar_cost = m.fs.costing.bar_cost
-    assert isinstance(bar_cost, pyo.Expression)
-    assert pyo.value(bar_cost) == 0.21
+    # Fix remaining variables.
+    # Using the .fix() method on a Var fixes the variable to its initialized value.
+    ix.resin_diam.fix()
+    ix.resin_bulk_dens.fix()
+    ix.bed_porosity.fix()
+    ix.dimensionless_time.fix()
 
-    m.fs.costing.bar_base_cost.value = 1.5
-    assert pyo.value(bar_cost) == 0.75
 
-    m.fs.costing.baz_cost = pyo.Var(initialize=5)
+def initialize_system(m):
+    # First we initialize the Feed block using values set in set_operating_conditions
+    m.fs.feed.initialize()
 
-    with pytest.raises(
-        RuntimeError,
-        match="Component baz_cost already exists on fs.costing but is not 42",
-    ):
-        m.fs.costing.register_flow_type(
-            "baz", 42 * pyo.units.USD_2020 / pyo.units.m**2 / pyo.units.day
-        )
-
-    m.fs.costing.flow_types.remove("baz")
-
-    m.fs.costing.register_flow_type(
-        "ham", 42 * pyo.units.USD_2021 / pyo.units.kg / pyo.units.minute
-    )
-
-    assert isinstance(m.fs.costing.ham_cost, pyo.Var)
-
-    m.fs.costing.cost_process()
-    # no error, wacc, plant_lifetime fixed
+    # We then propagate the state of the Feed block to the ion exchange model...
+    propagate_state(m.fs.feed_to_ix)
+    # ... and then initialize the ion exchange model.
+    m.fs.ion_exchange.initialize()
+    # With the ion exchange model initialized, we have initial guesses for the Product and Regen blocks
+    # and can propagate the state of the IX effluent and regeneration stream.
+    propagate_state(m.fs.ix_to_product)
+    propagate_state(m.fs.ix_to_regen)
+    # Finally, we initialize the product, regen and costing blocks.
+    m.fs.product.initialize()
+    m.fs.regen.initialize()
     m.fs.costing.initialize()
 
-    m.fs.costing.capital_recovery_factor.fix()
-    with pytest.raises(
-        RuntimeError,
-        match="Exactly two of the variables fs.costing.plant_lifetime, "
-        "fs.costing.wacc, fs.costing.capital_recovery_factor should be "
-        "fixed and the other unfixed.",
-    ):
-        # error, capital_recovery_factor,  wacc, plant_lifetime all fixed
-        m.fs.costing.initialize()
-    m.fs.costing.wacc.unfix()
-
-    # no error
-    m.fs.costing.initialize()
 
 def optimize_system(m):
     # Example of optimizing number of IX columns based on desired effluent equivalent concentration
@@ -219,40 +332,28 @@ def display_results(m):
     print(
         f'{f"Breakthrough/Initial Conc. [{target_ion}]":<40s}{ix.c_norm[target_ion]():<40.3%}'
     )
-
-    assert_units_consistent(m)
-
-    m.fs.costing.initialize()
-
-    total_LCOW = pyo.value(m.fs.costing.LCOW)
-
-    summed_aggregates = pyo.value(
-        sum(m.fs.costing.LCOW_aggregate_direct_capex.values())
-        + sum(m.fs.costing.LCOW_aggregate_indirect_capex.values())
-        + sum(m.fs.costing.LCOW_aggregate_fixed_opex.values())
-        + sum(m.fs.costing.LCOW_aggregate_variable_opex.values())
-        # electricity is counted both in the aggregate variable opex
-        # per unit and per flow, so it is double-counted in this sum
-        - m.fs.costing.LCOW_aggregate_variable_opex["electricity"]
+    print(
+        f'{"Vol. Flow In [m3/s]":<40s}{prop_in.flow_vol_phase[liq]():<40.5f}{"m3/s":<40s}'
     )
-    assert pytest.approx(total_LCOW) == summed_aggregates
-
-    summed_components = pyo.value(
-        sum(m.fs.costing.LCOW_component_direct_capex.values())
-        + sum(m.fs.costing.LCOW_component_indirect_capex.values())
-        + sum(m.fs.costing.LCOW_component_fixed_opex.values())
-        + sum(m.fs.costing.LCOW_component_variable_opex.values())
+    print(
+        f'{"Vol. Flow Out [m3/s]":<40s}{prop_out.flow_vol_phase[liq]():<40.5f}{"m3/s":<40s}'
     )
-    assert pytest.approx(total_LCOW) == summed_components
+    print(f'{"Water Vol. Recovery":<40s}{recovery:<40.2%}{"%":<40s}')
+    print(f'{"Breakthrough Time [hr]":<40s}{ix.t_breakthru() / 3600:<40.3f}{"hr":<40s}')
+    print(f'{"Number Columns":<40s}{ix.number_columns():<40.2f}{"---":<40s}')
+    print(f'{"Column Vol.":<40s}{ix.col_vol_per():<40.2f}{"m3":<40s}')
+    print(f'{"Bed Depth":<40s}{ix.bed_depth():<40.2f}{"m":<40s}')
+    for ion in ion_set:
+        print(
+            f'{f"Removal [{ion}]":<40s}{1 - prop_out.conc_mass_phase_comp[liq, ion]() / prop_in.conc_mass_phase_comp[liq, ion]():<40.4%}{"%":<40s}'
+        )
+        print(
+            f'{f"Conc. In [{ion}, mg/L]":<40s}{pyunits.convert(prop_in.conc_mass_phase_comp[liq, ion], to_units=pyunits.mg/pyunits.L)():<40.3e}{"mg/L":<40s}'
+        )
+        print(
+            f'{f"Conc. Out [{ion}, mg/L]":<40s}{pyunits.convert(prop_out.conc_mass_phase_comp[liq, ion], to_units=pyunits.mg/pyunits.L)():<40.3e}{"mg/L":<40s}'
+        )
 
-    sec = pyo.value(m.fs.costing.specific_energy_consumption)
-    summed_sec = pyo.value(
-        sum(m.fs.costing.specific_energy_consumption_component.values())
-    )
-    assert pytest.approx(sec) == summed_sec
 
-    seci = pyo.value(m.fs.costing.specific_electrical_carbon_intensity)
-    summed_seci = pyo.value(
-        sum(m.fs.costing.specific_electrical_carbon_intensity_component.values())
-    )
-    assert pytest.approx(seci) == summed_seci
+if __name__ == "__main__":
+    m = main()
